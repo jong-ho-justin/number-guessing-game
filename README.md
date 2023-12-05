5a08ae33993bdb424d1db4adb5644a052f2a56eb
jong-ho-justin <jong.ho.ca@gmail.com>
2023-12-04 23:24:54 -0800

    add ability to exit game

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Game released. Basically impossible to win; like 5% chance. Good luck.
 
 version 2.0 (12-04-2023)
 - added ability to choose guessing range
+
+version 2.1 (12-04-2023)
+- added option to exit game while in the guessing stage
+- changed winning message
+- fixed bug where it wasn't counting the attempt if you picked the correct answer
--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game():
         self.__answer = self.generate_random(self.__start, self.__end)
         self.__attempts = 0
         self.__game_won = False
+        self.__game_exit = False
 
     # RNG to generate the answer
     def generate_random(self, start, end) -> int:
@@ -18,21 +19,26 @@ class Game():
 
     # just a method that welcomes the player when game is initialized
     def start_message(self):
-        print("Welcome. This is a game where you guess a random number from a range you pick. You have 5 chance. Goodluck!\n")
+        print("Welcome. This is a game where you guess a random number from a range you pick. You have 5 chance. Goodluck!\nYou can type 'exit' to leave while guessing.\n")
 
     # method for starting game, loops the game_round method until either self.__game_won turns true or until 5 attempts are made
     def game_start(self):
-        while self.__game_won == False and self.__attempts < 5:
+        while self.__game_won == False and self.__attempts < 5 and self.__game_exit == False:
             self.game_round()
-        if self.__game_won == False and self.__attempts < 5:
+        if self.__game_won == False and self.__attempts < 5 and self.__game_exit == False:
             print(f"You lost. The answer was {self.__answer}.")
+        elif self.__game_exit:
+            print("You have exited the game.")
         else:
-          print(f"You win! The answer was {self.__answer}. Your attempt count was {self.__attempts}.")
+          print(f"You win! The answer was {self.__answer}. Attempts: {self.__attempts}.")
 
     # method for a round of game. calls the player_guess method then calls check_if_won method, passing player's guess
     def game_round(self):
         player_guess = self.get_players_guess()
-        if self.check_if_won(player_guess):
+        if player_guess == None:
+            self.__game_exit = True
+        elif self.check_if_won(player_guess):
+            self.__attempts += 1
             self.__game_won = True
         else:
             self.__attempts += 1
@@ -79,11 +85,12 @@ class Game():
         while need_valid_input:
             player_input = input(f"Guess a number from {self.__start} to {self.__end}: ")
 
-            # in case player just wants to exit
+            # in case player just wants to exit (doesn't work yet 12-04-2023)
             if player_input.lower() == "exit":
                 player_input = None
-                need_valid_input = True
-
+                need_valid_input = False
+                break
+            
             # checking if palyer input were valid numbers
             if player_input.isnumeric():
                 player_input = int(player_input)
