28eb17610a5199f462b09e44c0b402894ccd9ee5
jong-ho-justin <jong.ho.ca@gmail.com>
2023-12-04 23:07:04 -0800

    create player range mechanic

--- game.py
+++ game.py
@@ -1,38 +1,72 @@
-from random import random
+import random
 
 class Game():
     def __init__(self):
         self.start_message()
-        self.__answer = self.generate_random()
+        self.__start, self.__end = self.get_player_range()
+        self.__answer = self.generate_random(self.__start, self.__end)
         self.__attempts = 0
         self.__game_won = False
 
     # RNG to generate the answer
-    def generate_random(self) -> int:
-        return int(random() * 100)
+    def generate_random(self, start, end) -> int:
+        return random.randrange(start, end+1)
     
-    # ------------- for troubleshooting, delete after
+    # ------------- for troubleshooting
     def temp_print_answer(self):
         print(self.__answer)
 
     # just a method that welcomes the player when game is initialized
     def start_message(self):
-        print("Welcome. Guess a game from 0-100. You have 5 chance. Goodluck!")
+        print("Welcome. Guess a game from 0-100. You have 5 chance. Goodluck!\n")
 
     # method for starting game, loops the game_round method until either self.__game_won turns true or until 5 attempts are made
     def game_start(self):
         while self.__game_won == False and self.__attempts < 5:
             self.game_round()
+        if self.__game_won == False and self.__attempts < 5:
+            print(f"You lost. The answer was {self.__answer}.")
+        else:
+          print(f"You win! The answer was {self.__answer}. Your attempt count was {self.__attempts}.")
 
     # method for a round of game. calls the player_guess method then calls check_if_won method, passing player's guess
     def game_round(self):
         player_guess = self.get_players_guess()
         if self.check_if_won(player_guess):
             self.__game_won = True
-            print(f"You win! The answer was {self.__answer}. Your attempt count was {self.__attempts}.")
         else:
             self.__attempts += 1
             print(f"Darn, it wasn't {player_guess}! You have {5-self.__attempts} attempts left. \n")
+    
+    def get_player_range(self) -> int:
+        need_valid_input = True
+
+        while need_valid_input:
+            start_range = input("What range would you like to guess from? From: ")
+
+            # checking if palyer input were valid numbers
+            if start_range.isnumeric():
+                start_range = int(start_range)
+                need_valid_input = False
+            else:
+                print("That is not a valid input.\n")
+
+        need_valid_input = True
+
+        while need_valid_input:
+            end_range = input("To: ")
+
+            # checking if palyer input were valid numbers
+            if end_range.isnumeric():
+                end_range = int(end_range)
+                if end_range > start_range:
+                    need_valid_input = False
+                else:
+                    print(f"Needs to be bigger than the starting number of {start_range}.\n")
+            else:
+                print("That is not a valid input.\n")
+
+        return start_range, end_range
 
     # method for acquiring and validating player's input/player's guess
     def get_players_guess(self) -> int:
@@ -40,7 +74,7 @@ class Game():
 
         # loops until a valid input has been found
         while need_valid_input:
-            player_input = input("Guess a number from 0 to 100: ")
+            player_input = input(f"Guess a number from {self.__start} to {self.__end}: ")
 
             # in case player just wants to exit
             if player_input.lower() == "exit":
@@ -50,7 +84,7 @@ class Game():
             # checking if palyer input were valid numbers
             if player_input.isnumeric():
                 player_input = int(player_input)
-                if 0 <= player_input <= 100:
+                if self.__start <= player_input <= self.__end:
                     need_valid_input = False
                 else:
                     print("That is not a valid input.\n")
